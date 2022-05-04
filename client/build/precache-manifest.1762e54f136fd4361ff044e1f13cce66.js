81b725f2c62510cfee4496b2c27ed6e6f6d19d39
Bhargava-krishna <60109609+Bhargava-krishna@users.noreply.github.com>
2022-05-04 20:29:28 +0530

    Add files via upload

---
+++ client/build/precache-manifest.1762e54f136fd4361ff044e1f13cce66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b92f05baf0496e382df77a0a7b6794e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f21dea83cfaefc3bff6",
+    "url": "/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "13ed61fc12670cee20f8",
+    "url": "/static/css/main.84b2b9a5.chunk.css"
+  },
+  {
+    "revision": "7f21dea83cfaefc3bff6",
+    "url": "/static/js/2.edb40f98.chunk.js"
+  },
+  {
+    "revision": "13ed61fc12670cee20f8",
+    "url": "/static/js/main.909e7dc1.chunk.js"
+  },
+  {
+    "revision": "92c89455b99cd109be4a",
+    "url": "/static/js/runtime-main.b5d1efea.js"
+  }
+]);
